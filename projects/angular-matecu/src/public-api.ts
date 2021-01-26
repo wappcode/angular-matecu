57712d1b8fac865aab98b3b4f8d9455ed7053d17
JJesus <jjesus@iMac-de-JJesus.local>
2021-01-25 21:32:35 -0600

    se agregaron los archivos de paginated-list a public-api

--- projects/angular-matecu/src/public-api.ts
+++ projects/angular-matecu/src/public-api.ts
@@ -8,3 +8,9 @@
 export * from './lib/modules/matecu-spinner/matecu-spinner.module';
 export * from './lib/modules/matecu-spinner/components/matecu-spinner/matecu-spinner.component';
 export * from './lib/modules/matecu-spinner/services/matecu-spinner.service';
+ /**
+  * Modulo paginated-list
+  */
+export * from './lib/modules/matecu-paginated-list/matecu-paginated-list.module';
+export * from './lib/modules/matecu-paginated-list/components/matecu-paginated-list/matecu-paginated-list.component';
+export * from './lib/modules/matecu-paginated-list/services/matecu-abstract-list-service';
