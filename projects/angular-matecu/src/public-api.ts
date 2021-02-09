9c58be972c7bbceee4c53edb9573c04819bfdce4
JJesus <jjesus@iMac-de-JJesus.local>
2021-02-09 10:58:47 -0600

    se agrego topbar-layout a public-api

--- projects/angular-matecu/src/public-api.ts
+++ projects/angular-matecu/src/public-api.ts
@@ -2,15 +2,26 @@
  * Public API Surface of angular-matecu
  */
 
- /**
-  * Modulo Spinner
-  */
+/**
+ * Modulo Spinner
+ */
 export * from './lib/modules/matecu-spinner/matecu-spinner.module';
 export * from './lib/modules/matecu-spinner/components/matecu-spinner/matecu-spinner.component';
 export * from './lib/modules/matecu-spinner/services/matecu-spinner.service';
- /**
-  * Modulo paginated-list
-  */
-export * from './lib/modules/matecu-paginated-list/matecu-paginated-list.module';
-export * from './lib/modules/matecu-paginated-list/components/matecu-paginated-list/matecu-paginated-list.component';
-export * from './lib/modules/matecu-paginated-list/services/matecu-abstract-list-service';
+
+/**
+ * Módulo topbar-layout
+ */
+
+export * from './lib/modules/matecu-topbar-layout/matecu-topbar-layout.module';
+export * from './lib/modules/matecu-topbar-layout/components/matecu-topbar-body/matecu-topbar-body.component';
+export * from './lib/modules/matecu-topbar-layout/components/matecu-topbar-fab/matecu-topbar-fab.component';
+export * from './lib/modules/matecu-topbar-layout/components/matecu-topbar-layout/matecu-topbar-layout.component';
+export * from './lib/modules/matecu-topbar-layout/components/matecu-topbar-title/matecu-topbar-title.component';
+
+/**
+ * Modulo paginated-list
+ */
+// export * from './lib/modules/matecu-paginated-list/matecu-paginated-list.module';
+// export * from './lib/modules/matecu-paginated-list/components/matecu-paginated-list/matecu-paginated-list.component';
+// export * from './lib/modules/matecu-paginated-list/services/matecu-abstract-list-service';
